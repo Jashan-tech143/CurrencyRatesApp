52037466e4cb837b30d0be037c6c10fe7f97b6ae
Jashan-tech143 <jashanpreetkaur110424@gmail.com>
2025-01-11 01:00:31 -0600

    Added App component to render CurrencyRates and display latest currency rates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CurrencyRates from "./CurrencyRates"; // Make sure to create this file
+
+const App = () => {
+  return (
+    <div>
+      <h1>Latest Currency Rates</h1>
+      <CurrencyRates />
+    </div>
+  );
+};
+//export default
+export default App;
