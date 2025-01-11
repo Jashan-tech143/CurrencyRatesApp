5cec25995d2b6f7540311b4a410b2f8fef00670d
Jashan-tech143 <jashanpreetkaur110424@gmail.com>
2025-01-11 01:10:10 -0600

    Added App, CurrencyRates components, and styles for currency rates app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import CurrencyRates from "./CurrencyRates"; // Make sure to create this file
 
 const App = () => {
-  return (
-    <div>
-      <h1>Latest Currency Rates</h1>
-      <CurrencyRates />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Latest Currency Rates</h1>
+            <CurrencyRates />
+        </div>
+    );
 };
 //export default
 export default App;
